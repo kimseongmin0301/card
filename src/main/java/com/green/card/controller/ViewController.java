9b203eb6dc7f6cab8d9d4eea7dfb9258c835d470
kimseongmin0301 <pose1358@naver.com>
2022-10-04 16:20:22 +0900

    초기설정

--- src/main/java/com/green/card/controller/ViewController.java
+++ src/main/java/com/green/card/controller/ViewController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("/")
+    @GetMapping("/index1")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/index2")
+    public String index2() {
+        return "index2";
     }
 }
---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#header").load("/header.html");  //헤더 인클루드
+    $("#footer").load("/templates/common/footer.html");  //푸터부분 인클루드
+    console.log(1)
+})
